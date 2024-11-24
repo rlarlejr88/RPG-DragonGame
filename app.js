eeaef4d1fa7cfaef7f5365c751f32786ab5c2699
rlarlejr88 <kimkdluk@gmail.com>
2024-11-24 03:18:15 -0800

    commit-5

--- app.js
+++ app.js
@@ -4,60 +4,70 @@ let gold = 50;
 let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
-let inventory = ["stick"];
+let inventory = ['stick'];
 
 const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const text = document.querySelector('#text');
+const xpText = document.querySelector('#xpText');
+const healthText = document.querySelector('#healthText');
+const goldText = document.querySelector('#goldText');
+const monsterStats = document.querySelector('#monsterStats');
+const monsterName = document.querySelector('#monsterName');
+const monsterHealthText = document.querySelector('#monsterHealth');
 const weapons = [
   { name: 'stick', power: 5 },
   { name: 'dagger', power: 30 },
   { name: 'claw hammer', power: 50 },
-  { name: 'sword', power: 100 }
+  { name: 'sword', power: 100 },
 ];
 const monsters = [
   {
-    name: "slime",
+    name: 'slime',
     level: 2,
-    health: 15
+    health: 15,
   },
   {
-    name: "fanged beast",
+    name: 'fanged beast',
     level: 8,
-    health: 60
+    health: 60,
   },
   {
-    name: "dragon",
+    name: 'dragon',
     level: 20,
-    health: 300
-  }
-]
+    health: 300,
+  },
+];
 const locations = [
   {
-    name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"Store\"."
+    name: 'town square',
+    'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
+    'button functions': [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
   },
   {
-    name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store."
+    name: 'store',
+    'button text': [
+      'Buy 10 health (10 gold)',
+      'Buy weapon (30 gold)',
+      'Go to town square',
+    ],
+    'button functions': [buyHealth, buyWeapon, goTown],
+    text: 'You enter the store.',
   },
   {
-    name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters."
-  }
+    name: 'cave',
+    'button text': ['Fight slime', 'Fight fanged beast', 'Go to town square'],
+    'button functions': [fightSlime, fightBeast, goTown],
+    text: 'You enter the cave. You see some monsters.',
+  },
+  {
+    name: 'fight',
+    'button text': ['Attack', 'Dodge', 'Run'],
+    'button functions': [attack, dodge, goTown],
+    text: 'You are fighting a monster.',
+  },
 ];
 
 // initialize buttons
@@ -66,12 +76,12 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  button1.innerText = location["button text"][0];
-  button2.innerText = location["button text"][1];
-  button3.innerText = location["button text"][2];
-  button1.onclick = location["button functions"][0];
-  button2.onclick = location["button functions"][1];
-  button3.onclick = location["button functions"][2];
+  button1.innerText = location['button text'][0];
+  button2.innerText = location['button text'][1];
+  button3.innerText = location['button text'][2];
+  button1.onclick = location['button functions'][0];
+  button2.onclick = location['button functions'][1];
+  button3.onclick = location['button functions'][2];
   text.innerText = location.text;
 }
 
@@ -94,7 +104,7 @@ function buyHealth() {
     goldText.innerText = gold;
     healthText.innerText = health;
   } else {
-    text.innerText = "You do not have enough gold to buy health.";
+    text.innerText = 'You do not have enough gold to buy health.';
   }
 }
 
@@ -105,15 +115,15 @@ function buyWeapon() {
       currentWeaponIndex++;
       goldText.innerText = gold;
       let newWeapon = weapons[currentWeaponIndex].name;
-      text.innerText = "You now have a " + newWeapon + ".";
+      text.innerText = 'You now have a ' + newWeapon + '.';
       inventory.push(newWeapon);
-      text.innerText += " In your inventory you have: " + inventory;
+      text.innerText += ' In your inventory you have: ' + inventory;
     } else {
-      text.innerText = "You do not have enough gold to buy a weapon.";
+      text.innerText = 'You do not have enough gold to buy a weapon.';
     }
   } else {
-    text.innerText = "You already have the most powerful weapon!";
-    button2.innerText = "Sell weapon for 15 gold";
+    text.innerText = 'You already have the most powerful weapon!';
+    button2.innerText = 'Sell weapon for 15 gold';
     button2.onclick = sellWeapon;
   }
 }
@@ -123,8 +133,8 @@ function sellWeapon() {
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
-    text.innerText = "You sold a " + currentWeapon + ".";
-    text.innerText += " In your inventory you have: " + inventory;
+    text.innerText = 'You sold a ' + currentWeapon + '.';
+    text.innerText += ' In your inventory you have: ' + inventory;
   } else {
     text.innerText = "Don't sell your only weapon!";
   }
@@ -136,15 +146,30 @@ function fightSlime() {
 }
 
 function fightBeast() {
-    fighting = 1;
-    goFight();
+  fighting = 1;
+  goFight();
 }
 
 function fightDragon() {
-  fighting =2;
+  fighting = 2;
   goFight();
 }
 
 function goFight() {
-
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = 'block';
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
+
+function attack() {
+  text.innerText = 'The ' + monsters[fighting].name + ' attacks.';
+  text.innerText +=
+    ' You attack it with your ' + weapons[currentWeaponIndex].name + '.';
+  health -= monsters[fighting].level;
+  monsterHealth -=
+    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+}
+
+function dodge() {}
